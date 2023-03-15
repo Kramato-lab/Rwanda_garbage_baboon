c28e224b33fa56d275472a2e7f5cf63741cc9cfd
Kramato <54412528+Kramato-lab@users.noreply.github.com>
2023-03-14 22:30:07 -0500

    Update baboon_stats_for_pub.R

--- baboon_stats_for_pub.R
+++ baboon_stats_for_pub.R
@@ -64,6 +64,14 @@ adonis2(w2~access_trash, data=map2, by="margin", strata=year, permutations=5000)
 pairwise.adonis2(uw2~access_trash, strata="year", perm=5000, data=map2, p.adjust.m = 'holm')
 pairwise.adonis2(w2~access_trash, strata="year", perm=5000, data=map2, p.adjust.m = 'holm')
 
+uwdisp<-betadisper(uw2, map2$access_trash)
+anova(uwdisp)
+TukeyHSD(uwdisp)
+
+wdisp<-betadisper(w2, map2$access_trash)
+anova(wdisp)
+TukeyHSD(wdisp)
+
 #alpha diversity
 library(car)
 alpha<-read.csv('alpha-diversity-all.csv', header=T)
@@ -181,85 +189,128 @@ phyl_trash_corrected = bind_cols(phyl_trash[,1:2],
                                                         plot = F)))
 write_csv(phyl_trash_corrected, "phyl_trash_corrected.csv")
 
+##ANCOM
+library(tidyverse)
+library(phyloseq)
+library(ANCOMBC)
+
+setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Documents/Projects/Diakiw_baboons/KRA_analysis")
+asv = read.table("asv_table_abc.txt", header=T, check.names=FALSE)
+metadata_full = read.table("baboonmetadata_abc.txt", header=T)
+taxonomy2 = read.table("taxonomy2-abc.txt", header=T)
+
+asv_matrix = asv %>% column_to_rownames("sampleid") %>% as.matrix()
+tax_matrix2 = taxonomy2 %>% column_to_rownames("feature") %>% as.matrix()
+meta_df = metadata_full %>% column_to_rownames("SampleID")
+
+ASV<-otu_table(asv_matrix, taxa_are_rows = TRUE)
+TAX2<-tax_table(tax_matrix2)
+samples<-sample_data(meta_df)
+
+asv_phylo = phyloseq(ASV, TAX2, samples)
+
+###ASV ----
+
+trash_asv = ancombc2(data=asv_phylo, fix_formula="access_trash+Year",
+                     p_adj_method = "fdr", lib_cut = 10000,
+                     group = "access_trash", global = T)
+
+res_t_asv<-trash_asv$res
+res_global_t_asv<-trash_asv$res_global
+
+write_csv(res_t_asv, "Diff_abund_trash_asv.csv")
+write_csv(res_global_t_asv, "Diff_abund_trash_asv_global.csv")
+
+###Genus ----
+
+trash_genus = ancombc2(data=asv_phylo, fix_formula="access_trash+Year",
+                       tax_level="genus",
+                       p_adj_method = "fdr", lib_cut = 10000,
+                       group = "access_trash", global = T)
+
+res_t_genus<-trash_genus$res
+res_global_t_genus<-trash_genus$res_global
+
+write_csv(res_t_genus, "Diff_abund_trash_genus.csv")
+write_csv(res_global_t_genus, "Diff_abund_trash_genus_global.csv")
+
+##Family ----
+
+trash_fam = ancombc2(data=asv_phylo, tax_level = "family",
+                     fix_formula="access_trash+Year",
+                     p_adj_method = "fdr", lib_cut = 10000,
+                     group = "access_trash", global = T)
+
+res_t_fam<-trash_fam$res
+res_global_t_fam<-trash_fam$res_global
+
+write_csv(res_t_fam, "Diff_abund_trash_family.csv")
+write_csv(res_global_t_fam, "Diff_abund_trash_family_global.csv")
+
 #NMDS plots
 library(vegan)
 library(ggplot2)
-library(data.table)
+setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Documents/Projects/Diakiw_baboons/KRA_analysis/core-diversit-even11267")
 #Unweighted
 #Read in distance matrix and metadata file
-unweighted = read.table("unweighted-distance-matrix.tsv", header=T, check.names = FALSE)
-unweighted = as.dist(unweighted[,2:104])
-metadata<-fread("updated_baboonmetadata.txt", header=T)
+unweighted = read.table("uw-distance-matrix.tsv", header=T, check.names = FALSE)
+metadata<-read.table("metadata_dm.txt", header=T)
+metadata$year<-as.factor(metadata$year)
 
 #Perform MDS with maximum of 100 iterations to look for simplest solution
-unweighted.mds <- metaMDS(unweighted, k=2, trymax=100)
+unweighted.mds <- metaMDS(unweighted, k=2, trymax=500)
 
 #Add the mapping data to the MDS points
 unweighted.mds.points <- unweighted.mds$points
 
-unweighted.mds.points2 <- merge(x = unweighted.mds.points, y = metadata, by.x = "row.names", by.y = "sample-id")
+unweighted.mds.points2 <- merge(x = unweighted.mds.points, y = metadata, by.x = "row.names", by.y = "sampleid")
 
 #Plot MDS colored by access to trash
-nmds<-ggplot(unweighted.mds.points2,  aes(x = MDS1, y = MDS2, color=access_trash)) +
-  geom_point(size=4)+theme(panel.background = element_rect(fill = 'white', colour = 'black'))
-nmds
+nmds<-ggplot(unweighted.mds.points2,  aes(x = MDS1, y = MDS2, fill=access_trash, shape=year)) +
+  geom_point(size=4)+scale_fill_manual(values = c("gray","white", "black"))+scale_shape_manual(values=c(21,24))+
+  theme(panel.background = element_rect(fill = 'white', colour = 'black')) +
+  stat_ellipse(aes(group=access_trash), type='t')
 
-#manually add colors 
-nmds<-ggplot(unweighted.mds.points2,  aes(x = MDS1, y = MDS2, color=access_trash)) +  geom_point(size=4)+ 
-  scale_color_manual(values = c("#ff7f00","#4daf4a", "#377eb8","#984ea3"))+
-  theme(panel.background = element_rect(fill = 'white', colour = 'black'))
 nmds
-
-#ellipses
-nmds<-ggplot(unweighted.mds.points2,  aes(x = MDS1, y = MDS2, color=access_trash)) + geom_point(size=4, stroke=2)+ scale_color_manual(values = c("#ff7f00","#4daf4a", "#377eb8","#984ea3"))+theme(panel.background = element_rect(fill = 'white', colour = 'black'))+stat_ellipse(aes(group=access_trash), type='t')
-nmds
-
-#make pretty without ellipses
-nmds<-ggplot(unweighted.mds.points2,  aes(x = MDS1, y = MDS2, color=access_trash)) + geom_point(size=4, stroke=2)+ scale_color_manual(values = c("#ff7f00","#4daf4a", "#377eb8","#984ea3"))+theme(panel.background = element_rect(fill = 'white', colour = 'black'))+ 
-  theme(axis.title.x=element_text(size=rel(2)), 
-        axis.title.y=element_text(size=rel(2)),
-        plot.title = element_text(size=rel(3)),
-        legend.title = element_text(size=rel(2)),
-        legend.text = element_text(size = rel(1.8))) + 
-  ggtitle("Unweighted UniFrac")
-nmds
+ggsave(plot = nmds, width = 12, height = 3, filename = "uw_plot.pdf")
 
 #Weighted
 #Read in distance matrix and mapping file
-weighted = read.table("weighted-distance-matrix.tsv", header=T, check.names = FALSE)
-weighted = as.dist(weighted[,2:104])
-metadata<-fread("updated_baboonmetadata.txt", header=T)
+weighted = read.table("w-distance-matrix.tsv", header=T, check.names = FALSE)
+metadata<-read.table("metadata_dm.txt", header=T)
+metadata$year<-as.factor(metadata$year)
 
 #Perform MDS with maximum of 100 iterations to look for simplest solution
-weighted.mds <- metaMDS(weighted, k=2, trymax=100)
+weighted.mds <- metaMDS(weighted, k=2, trymax=500)
 
 #Add the mapping data to the MDS points
 weighted.mds.points <- weighted.mds$points
 
-weighted.mds.points2 <- merge(x = weighted.mds.points, y = metadata, by.x = "row.names", by.y = "sample-id")
+weighted.mds.points2 <- merge(x = weighted.mds.points, y = metadata, by.x = "row.names", by.y = "sampleid")
 
 #Plot MDS colored by access to trash
-nmds<-ggplot(weighted.mds.points2,  aes(x = MDS1, y = MDS2, color=access_trash)) +  geom_point(size=4)+theme(panel.background = element_rect(fill = 'white', colour = 'black'))
-nmds
+nmds<-ggplot(weighted.mds.points2,  aes(x = MDS1, y = MDS2, fill=access_trash, shape=year)) +
+  geom_point(size=4)+scale_fill_manual(values = c("gray","white", "black"))+scale_shape_manual(values=c(21,24))+
+  theme(panel.background = element_rect(fill = 'white', colour = 'black')) +
+  stat_ellipse(aes(group=access_trash), type='t')
 
-#manually add colors 
-nmds<-ggplot(weighted.mds.points2,  aes(x = MDS1, y = MDS2, color=access_trash)) +  geom_point(size=4)+ scale_color_manual(values = c("#ff7f00","#4daf4a", "#377eb8","#984ea3"))+theme(panel.background = element_rect(fill = 'white', colour = 'black'))
 nmds
+ggsave(plot = nmds, width = 12, height = 3, filename = "w_plot.pdf")
 
-#ellipses
-nmds<-ggplot(weighted.mds.points2,  aes(x = MDS1, y = MDS2, color=access_trash)) + geom_point(size=4, stroke=2)+ scale_color_manual(values = c("#ff7f00","#4daf4a", "#377eb8","#984ea3"))+theme(panel.background = element_rect(fill = 'white', colour = 'black'))+stat_ellipse(aes(group=access_trash), type='t')
-nmds
-
-#make pretty without ellipses
-nmds<-ggplot(weighted.mds.points2,  aes(x = MDS1, y = MDS2, color=access_trash)) + geom_point(size=4, stroke=2)+ scale_color_manual(values = c("#ff7f00","#4daf4a", "#377eb8","#984ea3"))+theme(panel.background = element_rect(fill = 'white', colour = 'black'))+ 
-  theme(axis.title.x=element_text(size=rel(2)), 
-        axis.title.y=element_text(size=rel(2)),
-        plot.title = element_text(size=rel(3)),
-        legend.title = element_text(size=rel(2)),
-        legend.text = element_text(size = rel(1.8))) + 
-  ggtitle("Weighted UniFrac")
-nmds
-
+#stacked barplot
+library(tidyverse)
+library(ggplot2)
+library(RColorBrewer)
+box<-read.table("family_for_boxplot.txt", header=T)
+box_long<-box %>% pivot_longer(cols=contains("f_"), names_to="taxon", values_to="value")
+nb.colors<-23
+mycolors<-colorRampPalette(brewer.pal(12,"Paired"))(nb.colors)
+fam_box<-ggplot(box_long, aes(fill=taxon, y=value, x=sample_order, color=taxon))+
+  geom_bar(position="stack", stat="identity")+
+  scale_fill_manual(values=mycolors)+
+  scale_color_manual(values=mycolors)+
+  theme(panel.background = element_rect(fill = 'white', colour = 'black'))
+fam_box
 
 #alpha diversity plots
 library(ggplot2)
